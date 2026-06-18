15fbe47d86f5390ab7615ed7e3c5e32c4d333097
masaoy_j <masaoy_j@f112.ub.lan>
2026-06-18 09:24:44 +0800

    feat(products): display products using reusable product cards

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PetShop } from '../pet-shop.service';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-products',
-  imports: [],
-  templateUrl: './products.html',
-  styleUrl: './products.css',
+  standalone: true,
+  imports: [CommonModule, ProductCardComponent],
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
 })
-export class Products {}
+export class ProductsComponent {
+  products = this.petShop.getProducts();
+
+  constructor(private petShopService: PetShop) {}
+}
