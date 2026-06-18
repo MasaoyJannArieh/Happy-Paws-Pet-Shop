6fe4ccc15a3476bb318e2eab6747c38827662d3b
masaoy_j <masaoy_j@f112.ub.lan>
2026-06-18 10:02:10 +0800

    feat(router): add routes for home, products, cart, checkout, about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home';
+import { ProductsComponent } from './products/products';
+import { CartComponent } from './cart/cart';
+import { CheckoutComponent } from './checkout/checkout';
+import { AboutComponent } from './about/about';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'about', component: AboutComponent },
+
+  { path: '**', redirectTo: 'home' }
+];
